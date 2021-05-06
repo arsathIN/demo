a610b7352420ee8635ac561f0147ae4c3c3e333c
arsathIN <arsathINparvez@gmail.com>
2021-05-07 00:17:33 +0530

    added

--- src/org/demo/Demo_class.java
+++ src/org/demo/Demo_class.java
@@ -4,6 +4,11 @@ public class Demo_class {
 		public void gta() {
 			System.out.println("gta");
 		}
+		
+		public void gta1() {
+			System.out.println("gta1");
+		}
+		
 		public static void main(String[] args) {
 			Demo_class n=new Demo_class();
 			n.gta();
