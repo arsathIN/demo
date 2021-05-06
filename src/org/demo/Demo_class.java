52feb16495f7a007b6f01e951b6b5e67ab7168e7
arsathIN <arsathINparvez@gmail.com>
2021-05-06 21:43:48 +0530

    please work

---
+++ src/org/demo/Demo_class.java
@@ -0,0 +1,13 @@
+package org.demo;
+
+public class Demo_class {
+		public void gta() {
+			System.out.println("gta");
+		}
+		public static void main(String[] args) {
+			Demo_class n=new Demo_class();
+			n.gta();
+		}
+	}
+
+
